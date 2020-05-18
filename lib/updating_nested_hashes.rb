721fdd221886c0411eb28e5f47c8e5b673615dff
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-18 20:44:12 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -17,11 +17,6 @@ def update_status
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
-      }
-      :additional_characters => {
-          :firstchar => {name: "Prince Escalus"}
-          :secondchar => {name: "Apothecary"}
-      }
    }
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
